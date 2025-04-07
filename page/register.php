51e071a16680ffadb61a4754f6cfca3b6b580069
Andi Ahmad Nurmadani <andiahmadnurmadani15@gmail.com>
2025-04-07 10:23:37 +0700

    rename register.css to styleRegister.css

--- page/register.php
+++ page/register.php
@@ -8,8 +8,8 @@ include "../include/conn.php";
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Daftar Akun Baru</title>
+  <link rel="stylesheet" href="../assets/styleRegister.css">
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="../assets/register.css">
 </head>
 
 <body>
